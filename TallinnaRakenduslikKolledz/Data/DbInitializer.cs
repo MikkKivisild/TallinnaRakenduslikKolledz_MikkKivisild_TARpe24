91e4b4bfba04f96ef310f76c65976ce6f8dabe0c
Opilane TTHK <opilane@tthk.ee>
2025-09-04 13:21:46 +0300

    Data

---
+++ TallinnaRakenduslikKolledz/Data/DbInitializer.cs
@@ -0,0 +1,29 @@
+﻿using TallinnaRakenduslikKolledz.Models;
+
+namespace TallinnaRakenduslikKolledz.Data
+{
+    public class DbInitializer
+    {
+        public static void Initializer(SchoolContext context)
+        {
+            context.Database.EnsureCreated();
+            if(context.Students.Any())
+            { 
+                return; 
+            }
+
+            var students = new Student[]
+            {
+                new Student
+                {
+                    FirstName = "sdf",
+                    LastName = "AAA",
+                    EnrollmentDate = DateTime.Now,
+                    Gender = "M",
+                    Age = 21
+                    EnrollmentStatus = "A"
+                }
+            };
+        }
+    }
+}
