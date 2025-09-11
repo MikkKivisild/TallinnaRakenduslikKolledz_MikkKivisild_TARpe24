578ef64c846923c12113f2175b2f18e829fb5e16
MikkKivisild <159937176+MikkKivisild@users.noreply.github.com>
2025-09-11 13:16:26 +0300

    Edit View and Controller

--- TallinnaRakenduslikKolledz/Controllers/StudentsController.cs
+++ TallinnaRakenduslikKolledz/Controllers/StudentsController.cs
@@ -75,5 +75,40 @@ namespace TallinnaRakenduslikKolledz.Controllers
             }
             return View(student);
         }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = _context.Students.Find(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Student student)
+        {
+            if (id != student.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(student);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
     }
 }
