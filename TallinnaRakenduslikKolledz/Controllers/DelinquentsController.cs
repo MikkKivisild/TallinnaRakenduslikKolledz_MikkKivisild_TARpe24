e02f145c52b9c46f6c606b6082d7ac6f4b6736c8
Opilane TTHK <opilane@tthk.ee>
2025-10-15 10:49:04 +0300

    Done Delinquent model, added it to db and started on controller

---
+++ TallinnaRakenduslikKolledz/Controllers/DelinquentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TallinnaRakenduslikKolledz.Controllers
+{
+    public class DelinquentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
