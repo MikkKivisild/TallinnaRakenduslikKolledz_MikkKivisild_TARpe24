9e4df8876aeb70c589e715c8b21ad97c06566bc1
mikkm <mikkm@DESKTOP-6OR357K>
2025-10-20 20:01:51 +0300

    Departments Edit + Create

--- TallinnaRakenduslikKolledz/Controllers/DepartmentsController.cs
+++ TallinnaRakenduslikKolledz/Controllers/DepartmentsController.cs
@@ -60,5 +60,62 @@ namespace TallinnaRakenduslikKolledz.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            ViewData["action"] = "Edit";
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var department = _context.Departments.Find(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return View("Create", department);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([Bind("DepartmentID,Name,Budget,StartDate,RowVersion,InstructorID,Building,City,County")] Department department, int id)
+        {
+            department.DepartmentID = id;
+            if (id != department.DepartmentID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(department);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewData["action"] = "Create";
+            ViewData["InstructorID"] = new SelectList(_context.Instructors, "ID", "FullName");
+            return View("Create");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DepartmentID,Name,Budget,StartDate,RowVersion,InstructorID,Building,City,County")] Department department)
+        {
+            ViewData["action"] = "Create";
+            if (ModelState.IsValid)
+            {
+                _context.Add(department);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewData["InstructorID"] = new SelectList(_context.Instructors, "Id", "FullName", department.InstructorID);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
